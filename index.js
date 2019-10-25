b17cb6bbee857b79622ae6b5cafdf19a9a1bfd82
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-10-25 13:43:27 -0700

    Added type checking, fixes #3

--- index.js
+++ index.js
@@ -1,85 +1,82 @@
 
+function checkParams(params, _source) {
+  return async (req, res, next) => {
+    // Actual source
+    const source = req[_source];
+
+    for (const k in params) {
+      // If not present in body
+      if (!(k in source)) {
+        // If not optional
+        if (!params[k].optional)
+          return res.status(400).json({
+            error: params[k].msg || `Param '${k}' missing.`
+          });
+        // If it is optional skip it
+        else
+          continue ;
+      }
+
+      // If type was specified
+      if ('type' in params[k]) {
+        if (typeof params[k].type !== 'function')
+          throw new TypeError(
+            'Key type must be of type Function (e.g. Number, Array, ...)'
+          );
+
+        if (Object.prototype.toString.call(source[k]) !==
+            Object.prototype.toString.call(params[k].type())) {
+          // Value has wrong type
+          return res.status(400).json({
+            error: params[k].msg || `Invalid type for param '${k}'.`
+          });
+        }
+      }
+
+      // If validate function was specified
+      if (typeof params[k].validate === 'function') {
+        let _valid = await params[k].validate(source[k]);
+        if (_valid !== true) {
+          if (typeof _valid === 'string')
+            return res.status(400).json({ error: _valid });
+          return (
+            res.status(400).json({
+              error: params[k].msg || `Invalid param '${k}'.`
+            }));
+        }
+      }
+    }
+
+    next();
+  };
+}
+
 module.exports = {
   /**
    * @param {Object} params Object with required keys
    */
   reqparams(params) {
-    return async (req, res, next) => {
-      // Foreach key in params
-      for (const k in params) {
-        // If not present in body
-        if (!(k in req.body)) {
-          // If not optional
-          if (!params[k].optional)
-            return res.status(400).json({
-              error: params[k].msg || `Param '${k}' missing.`
-            });
-          // If it is optional skip it
-          else
-            continue ;
-        }
-
-        // If validate function was specified
-        if (typeof params[k].validate === 'function') {
-          let _valid = await params[k].validate(req.body[k]);
-          if (_valid !== true) {
-            if (typeof _valid === 'string')
-              return res.status(400).json({ error: _valid });
-            return (
-              res.status(400).json({
-                error: params[k].msg || `Invalid param '${k}'.`
-              }));
-          }
-        }
-      }
-      next();
-    };
+    return checkParams(params, 'body');
   },
 
   /**
    * @param {Object} params Object with required keys
    */
   reqquery(params) {
-    return async (req, res, next) => {
-      // Foreach key in params
-      for (const k in params) {
-        // If not present in query and not optional
-        if (!(k in req.query)) {
-          // If not optional
-          if (!params[k].optional)
-            return res.status(400).json({
-              error: params[k].msg || `Param '${k}' missing.`
-            });
-          // If it is optional skip it
-          else
-            continue ;
-        }
-
-        // If validate function was specified
-        if (typeof params[k].validate === 'function') {
-          let _valid = await params[k].validate(req.query[k]);
-          if (_valid !== true) {
-            if (typeof _valid === 'string')
-              return res.status(400).json({ error: _valid });
-            return (
-              res.status(400).json({
-                error: params[k].msg || `Invalid param '${k}'.`
-              }));
-          }
-        }
-      }
-      next();
-    };
+    return checkParams(params, 'query');
   },
 
   /**
    * Check if a string only contains whitespaces
-   * @param {String} str The string to be tested
+   * @param {String|Array} val The string or array to be tested
    *
    * @returns {Boolean}
    */
-  notEmpty(str) {
-    return (!/^\s*$/.test(str));
+  notEmpty(val) {
+    if (val instanceof Array)
+      return (val.length > 0);
+    else
+      return (!/^\s*$/.test(val));
   }
 
 };
