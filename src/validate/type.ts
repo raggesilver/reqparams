105711dd3658a04d5553dfe5de7dbfa527704cf4
Paulo Queiroz <pvaqueiroz@gmail.com>
2021-08-04 11:09:08 -0300

    Fix type checking

--- src/validate/type.ts
+++ src/validate/type.ts
@@ -1,15 +1,17 @@
-import { ValidateFunction } from '../types';
-
 import _ from '@raggesilver/hidash';
+
+import { ErrorType, ValidateFunction } from '../types';
+import { ParamError } from '../internal/error-defaults';
 
 // FIXME: the comparing logic for type conversion & validation should have it's
 // own function in order for other components to use it.
 
 export default function (): ValidateFunction {
   return (val: any, req, param, path, source) => {
+    const error = new ParamError(param, path, ErrorType.INVALID_PARAM_TYPE);
     // Check for null
     if (val === null && !param.nullable) {
-      return false;
+      throw error;
     }
     // Check if a date was provided as ISO-string or number. If so the payload
     // value will be converted to a JavaScript Date and the type checking will
@@ -27,7 +29,7 @@ export default function (): ValidateFunction {
     }
     // Check type
     if (toString.call(val) !== toString.call(new param.type())) {
-      return false;
+      throw error;
     }
     return true;
   };
